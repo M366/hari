9508ddf96b276d7c78e980ec1f0402f4d614c7bc
Masataka Mimuro <masataka.mimuro@gmail.com>
2020-06-26 21:05:05 +0900

    Step harib05b: Add mouse decoder
    page 157
    skip harib05a

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -3,15 +3,21 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+typedef struct MOUSE_DEC {
+    unsigned char buf[3], phase;
+} MOUSE_DEC;
+
 extern FIFO8 keyfifo, mousefifo;
-void enable_mouse(void);
+void enable_mouse(MOUSE_DEC *mdec);
 void init_keyboard(void);
+int mouse_decode(MOUSE_DEC *mdec, unsigned char dat);
 
 
 void HariMain(void) {
     BOOTINFO *binfo = (BOOTINFO *) ADR_BOOTINFO;
     char s[40], mcursor[256], keybuf[32], mousebuf[128];
     int mx, my, i;
+    MOUSE_DEC mdec;
 
     init_gdtidt();
     init_pic();
@@ -34,7 +40,7 @@ void HariMain(void) {
     sprintf(s, "(%d, %d)", mx, my);
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, 0, s);
 
-    enable_mouse();
+    enable_mouse(&mdec);
 
     for (;;) {
         io_cli(); // disenable interrupt
@@ -50,9 +56,11 @@ void HariMain(void) {
             } else if (fifo8_status(&mousefifo) != 0) {
                 i = fifo8_get(&mousefifo);
                 io_sti();
-                sprintf(s, "%02X", i);
-                boxfill8(binfo->vram, binfo->scrnx, 7, 32, 16, 47, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, 0, s);
+                if (mouse_decode(&mdec, i) != 0) {
+                    sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
+                    boxfill8(binfo->vram, binfo->scrnx, 7, 32, 16, 32 + 8 * 8 - 1, 31);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, 0, s);
+                }
             }
         }
     }
@@ -87,11 +95,40 @@ void init_keyboard(void) {
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
 
-void enable_mouse(void) {
+void enable_mouse(MOUSE_DEC *mdec) {
 	/* マウス有効 */
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	return; /* うまくいくとACK(0xfa)が送信されてくる */
+
+    // Receive ACK(0xfa) when success connecting to mouse
+    mdec->phase = 0; // wait 0xfa
+	return;
+}
+
+
+int mouse_decode(MOUSE_DEC *mdec, unsigned char dat) {
+    if (mdec->phase == 0) {
+        if (dat == 0xfa) {
+            mdec->phase = 1;
+        }
+        return 0;
+    }
+    if (mdec->phase == 1) {
+        mdec->buf[0] = dat;
+        mdec->phase = 2;
+        return 0;
+    }
+    if (mdec->phase == 2) {
+        mdec->buf[1] = dat;
+        mdec->phase = 3;
+        return 0;
+    }
+    if (mdec->phase == 3) {
+        mdec->buf[2] = dat;
+        mdec->phase = 1;
+        return 1;
+    }
+    return -1;
 }
