301f34ea2b7b2e6adf4061fd528f7c7063195d0e
Masataka Mimuro <masataka.mimuro@gmail.com>
2020-06-27 09:05:29 +0900

    Step harib05c: Modify mouse decoder to read motion and click
    page 161

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -5,6 +5,7 @@
 
 typedef struct MOUSE_DEC {
     unsigned char buf[3], phase;
+    int x, y, btn;
 } MOUSE_DEC;
 
 extern FIFO8 keyfifo, mousefifo;
@@ -57,8 +58,14 @@ void HariMain(void) {
                 i = fifo8_get(&mousefifo);
                 io_sti();
                 if (mouse_decode(&mdec, i) != 0) {
-                    sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-                    boxfill8(binfo->vram, binfo->scrnx, 7, 32, 16, 32 + 8 * 8 - 1, 31);
+                    sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+                    if ((mdec.btn & 0x01) != 0)
+                        s[1] = 'L';
+                    if ((mdec.btn & 0x02) != 0)
+                        s[3] = 'R';
+                    if ((mdec.btn & 0x04) != 0)
+                        s[2] = 'C';
+                    boxfill8(binfo->vram, binfo->scrnx, 7, 32, 16, 32 + 15 * 8 - 1, 31);
                     putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, 0, s);
                 }
             }
@@ -102,22 +109,26 @@ void enable_mouse(MOUSE_DEC *mdec) {
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
 
-    // Receive ACK(0xfa) when success connecting to mouse
-    mdec->phase = 0; // wait 0xfa
+    // Initialize mdec->phase. It will be receive ACK(0xfa) when success connecting to mouse
+    mdec->phase = 0; // 
 	return;
 }
 
 
 int mouse_decode(MOUSE_DEC *mdec, unsigned char dat) {
     if (mdec->phase == 0) {
+        // check first byte. The mouse send 0xfa when connecting mouse
         if (dat == 0xfa) {
             mdec->phase = 1;
         }
         return 0;
     }
     if (mdec->phase == 1) {
-        mdec->buf[0] = dat;
-        mdec->phase = 2;
+        // check first byte of motion
+        if ((dat & 0xc8) == 0x08) { // c8 = 1100 1000 : 1100 for not correct bit, 1000 always on.
+            mdec->buf[0] = dat;
+            mdec->phase = 2;
+        }
         return 0;
     }
     if (mdec->phase == 2) {
@@ -128,7 +139,17 @@ int mouse_decode(MOUSE_DEC *mdec, unsigned char dat) {
     if (mdec->phase == 3) {
         mdec->buf[2] = dat;
         mdec->phase = 1;
+        mdec->btn = mdec->buf[0] & 0x07;
+        mdec->x = mdec->buf[1];
+        mdec->y = mdec->buf[2];
+        if ((mdec->buf[0] & 0x10) != 0) {
+            mdec->x |= 0xffffff00;
+        }
+        if ((mdec->buf[0] & 0x20) != 0) {
+            mdec->y |= 0xffffff00;
+        }
+        mdec->y = - mdec->y; // The Y direction of the mouse is opposite of the display.
         return 1;
     }
-    return -1;
+    return -1; // error
 }
