5295bbf5a2ca5e4e00c9a9abdf5e3c603517c018
Masataka Mimuro <masataka.mimuro@gmail.com>
2020-07-10 17:12:39 +0900

    Step harb07d: Add sheet
    page 198 ~ 213

--- harib/bootpack.c
+++ harib/bootpack.c
@@ -5,16 +5,18 @@
 
 void HariMain(void) {
     BOOTINFO *binfo = (BOOTINFO *) ADR_BOOTINFO;
-    char s[40], mcursor[256], keybuf[32], mousebuf[128];
+    char s[40], keybuf[32], mousebuf[128];
     int mx, my, i;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	struct SHTCTL *shtctl;
+	struct SHEET *sht_back, *sht_mouse;
+	unsigned char *buf_back, buf_mouse[256];
 
     init_gdtidt();
     init_pic();
     io_sti(); // enable CPU interrupt. this process is done after the PIC initialization.
-
     fifo8_init(&keyfifo, 32, keybuf);
     fifo8_init(&mousefifo, 128, mousebuf);
     io_out8(PIC0_IMR, 0xf9); // enable PIC1 and keyboard (11111001)
@@ -28,17 +30,27 @@ void HariMain(void) {
 	memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
     init_palette();
-    init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+  	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+	sht_back  = sheet_alloc(shtctl);
+	sht_mouse = sheet_alloc(shtctl);
+	buf_back  = (unsigned char *) memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1); /* 透明色なし */
+	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+	init_mouse_cursor8(buf_mouse, 99);
+	sheet_slide(shtctl, sht_back, 0, 0);  
     mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;
-    init_mouse_cursor8(mcursor, 7);
-    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sheet_slide(shtctl, sht_mouse, mx, my);
+	sheet_updown(shtctl, sht_back,  0);
+	sheet_updown(shtctl, sht_mouse, 1);
     sprintf(s, "(%3d, %3d)", mx, my);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, 0, s);
+    putfonts8_asc(buf_back, binfo->scrnx, 0, 0, 0, s);
 
 	sprintf(s, "memory %dMB   free : %dKB",
 			memtotal / (1024 * 1024), memman_total(memman) / 1024);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, 0, s);
+	putfonts8_asc(buf_back, binfo->scrnx, 0, 32, 0, s);
+	sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 
     for (;;) {
         io_cli(); // disenable interrupt
@@ -49,8 +61,9 @@ void HariMain(void) {
                 i = fifo8_get(&keyfifo);
                 io_sti();
                 sprintf(s, "%02X", i);
-                boxfill8(binfo->vram, binfo->scrnx, 7, 0, 16, 15, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, 0, s);
+                boxfill8(buf_back, binfo->scrnx, 7, 0, 16, 15, 31);
+                putfonts8_asc(buf_back, binfo->scrnx, 0, 16, 0, s);
+                sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
             } else if (fifo8_status(&mousefifo) != 0) {
                 i = fifo8_get(&mousefifo);
                 io_sti();
@@ -62,8 +75,9 @@ void HariMain(void) {
                         s[3] = 'R';
                     if ((mdec.btn & 0x04) != 0)
                         s[2] = 'C';
-                    boxfill8(binfo->vram, binfo->scrnx, 7, 32, 16, 32 + 15 * 8 - 1, 31);
-                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, 0, s);
+                    boxfill8(buf_back, binfo->scrnx, 7, 32, 16, 32 + 15 * 8 - 1, 31);
+                    putfonts8_asc(buf_back, binfo->scrnx, 32, 16, 0, s);
+                    sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
 
                     // move mouse cursol
                     boxfill8(binfo->vram, binfo->scrnx, 7, mx, my, mx + 15, my + 15); // delete mouse cursol
@@ -80,9 +94,10 @@ void HariMain(void) {
                         my = binfo->scrny - 16;
                     
                     sprintf(s, "(%3d, %3d)", mx, my);
-                    boxfill8(binfo->vram, binfo->scrnx, 7, 0, 0, 79, 15); // delete mouse position
-                    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, 0, s); // draw mouse position
-                    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); // draw mouse cursol
+                    boxfill8(buf_back, binfo->scrnx, 7, 0, 0, 79, 15); // delete mouse position
+                    putfonts8_asc(buf_back, binfo->scrnx, 0, 0, 0, s); // draw mouse position
+					sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
+					sheet_slide(shtctl, sht_mouse, mx, my);
                 }
             }
         }
